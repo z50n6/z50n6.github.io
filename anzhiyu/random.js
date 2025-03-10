6477065b34915de511c452a2aac071e12ea43518
bypass <bypass@qq.com>
2025-03-10 17:41:58 +0800

    Site updated: 2025-03-10 17:41:57

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/10/hello-world/"];function toRandomPost(){
+var posts=["2025/03/10/hello-world/","2017/12/02/linux/Linux基本命令/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
