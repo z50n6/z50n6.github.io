3ae42d36424cfcdbb3f213d6c1936d74a98a6f9b
bypass <bypass@qq.com>
2025-03-10 17:49:59 +0800

    Site updated: 2025-03-10 17:49:58

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/10/hello-world/","2025/02/02/linux/Linux基本命令/","2025/02/02/linux/包过滤防火墙——iptables静态防火墙的简单使用/","2025/02/02/linux/包过滤防火墙——firewalld动态防火墙的简单使用/"];function toRandomPost(){
+var posts=["2024/02/02/linux/包过滤防火墙——iptables静态防火墙的简单使用/","2024/02/02/linux/包过滤防火墙——firewalld动态防火墙的简单使用/","2025/03/10/hello-world/","2024/02/02/linux/Linux基本命令/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
