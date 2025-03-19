6cd5d899c6dff1f8c9888b46c5ee77c6d3aab94e
saury <admin@qq.com>
2025-03-19 09:48:14 +0800

    Site updated: 2025-03-19 09:48:13

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/02/02/linux/Linux基本命令/","2024/02/02/linux/包过滤防火墙——firewalld动态防火墙的简单使用/","2024/02/02/linux/包过滤防火墙——iptables静态防火墙的简单使用/"];function toRandomPost(){
+var posts=["2024/02/02/linux/包过滤防火墙——iptables静态防火墙的简单使用/","2024/02/02/linux/Linux基本命令/","2024/02/02/linux/包过滤防火墙——firewalld动态防火墙的简单使用/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
